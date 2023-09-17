2bc76511669b07744e933e11146f7faa90f15c26
NahomKibreab <nknahom@gmail.com>
2023-09-17 12:14:25 -0600

    While loop

---
+++ while-loop.js
@@ -0,0 +1,13 @@
+console.log("Before while loop");
+
+let i = 0;
+
+const names = ["Nahom", "Mohammed", "Ahmed"];
+
+while (i < names.length) {
+  // do something
+  console.log("hi " + names[i]);
+  i++;
+}
+
+console.log("After while loop");
