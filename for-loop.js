c4bcc5988ac80f124a89595f63938adcf4c334f6
NahomKibreab <nknahom@gmail.com>
2023-09-17 12:00:42 -0600

    For loop

---
+++ for-loop.js
@@ -0,0 +1,12 @@
+// For loop
+
+console.log("Before for loop");
+
+const names = ["Nahom", "Mohammed", "Ahmed"];
+
+for (let i = 0; i < names.length; i++) {
+  // do something
+  console.log("Hi " + names[i]);
+}
+
+console.log("After for loop");
